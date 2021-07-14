de32e998d4597209a8d391f315080edbc7f33212
Mike Zamansky <zamansky@gmail.com>
2021-07-14 13:03:21 -0400

    renamed name to data

--- code/lists/Node.java
+++ code/lists/Node.java
@@ -3,43 +3,43 @@ import java.util.*;
 
 public class Node {
 
-  private String name;
-  private Node next;
+    private String data;
+    private Node next;
 
-  public Node() {
-    name = "";
-    next = null;
-  }//default constructor
+    public Node() {
+	data = "";
+	next = null;
+    }//default constructor
 
-  public Node(String value) {
+    public Node(String value) {
 
-    name = value;
-    next = null;
-  }//constructor(vale)
+	data = value;
+	next = null;
+    }//constructor(vale)
 
-  public Node(String value, Node next) {
-    name = value;
-    this.next = next;
-  }//constructor(value, next)
+    public Node(String value, Node next) {
+	data = value;
+	this.next = next;
+    }//constructor(value, next)
 
-  public void setData(String value) {
-    name = value;
-  }//setData
+    public void setData(String value) {
+	data = value;
+    }//setData
 
-  public void setNext(Node n) {
-    next = n;
-  }//setNext
+    public void setNext(Node n) {
+	next = n;
+    }//setNext
 
-  public String getValue() {
-    return name;
-  }//end getValue
+    public String getData() {
+	return data;
+    }//end getValue
 
-  public Node getNext() {
-    return next;
-  }//getNext
+    public Node getNext() {
+	return next;
+    }//getNext
 
-  public String toString() {
-    return name;
-  }//toString
+    public String toString() {
+	return data;
+    }//toString
 
 }//class Node
