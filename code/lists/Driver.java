ad799085db1f049d3eaf13f9b80ee9154febf246
Mike Zamansky <zamansky@gmail.com>
2021-07-14 13:15:16 -0400

    updated Driver with exercise

---
+++ code/lists/Driver.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+
+public class Driver{
+    public static void main(String[] args) {
+	Node n;
+	n = new Node();
+	n.setData("Eduardo");
+	
+
+	Node n2 = new Node("Brian");
+	n.setNext(n2);
+
+	System.out.println(n);
+
+	System.out.println(n2);
+
+	System.out.println(n.getNext());
+	n.getNext().setNext(new Node("Steph"));
+	System.out.println(n2.getNext());
+	System.out.println(n.getNext().getNext());
+	
+	Node L;	
+	// 1.Create a new  list that looks like:
+	//   L->"a"->"b"->"c"->"d"
+	// 2. Write the code to insert an "x"
+	//    between the b and the c
+	// 3. Write the code to delete the c
+
+	
+    }
+}
