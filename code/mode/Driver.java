c95426581f54f86d14c8e4d6e00de51e5b41e89e
Mike Zamansky <zamansky@gmail.com>
2021-07-22 11:30:09 -0400

    updated

--- code/mode/Driver.java
+++ code/mode/Driver.java
@@ -4,10 +4,15 @@ import java.util.*;
 public class Driver{
 
     public static void main(String[] args) {
-	Mode m = new Mode();
-
-	System.out.println(m);
-	System.out.println(m.calcMode());
+	long start,elapsed;
+	int mode;
+	Mode m = new Mode(100000);
+	start = System.currentTimeMillis();
+	mode = m.calcMode();
+	//System.out.println(m);
+	System.out.println(mode);
+	elapsed = System.currentTimeMillis()-start;
+	System.out.println(elapsed);
     }
     
 }
